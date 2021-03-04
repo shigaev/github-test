08fe437a331b5cb3e2438c6e3a654a9fefc53947
Shigaev Dmitriy <shigaev_92@mail.ru>
2021-03-04 19:15:05 +0300

    init: first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let text = document.querySelector('.text')
+let textLorem = 'Lorem text block'
+
+function showText() {
+    text.textContent = textLorem
+}
+
+text.addEventListener('click', showText)
+
